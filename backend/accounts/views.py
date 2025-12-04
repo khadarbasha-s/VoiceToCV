a260285223ee5562f58e01105054a159408b0f81
khadar basha <khadarbashasaddalapalli@gmail.com>
2025-12-05 00:21:28 +0530

    made changes in the signup page and added the roles

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,37 +1,34 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from .serializers import SignupSerializer, LoginSerializer, UserSerializer
+from .serializers import UserSerializer, SignupSerializer, LoginSerializer
+from django.contrib.auth.models import User
 
-@api_view(['POST'])
-def signup(request):
-    serializer = SignupSerializer(data=request.data)
-    if serializer.is_valid():
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = SignupSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         user = serializer.save()
         token, created = Token.objects.get_or_create(user=user)
-        
-        # Get user profile data
-        user_serializer = UserSerializer(user)
-        
         return Response({
-            'token': token.key,
-            'user': user_serializer.data
+            "user": UserSerializer(user).data,
+            "token": token.key
         }, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['POST'])
-def login(request):
-    serializer = LoginSerializer(data=request.data)
-    if serializer.is_valid():
+class LoginView(generics.GenericAPIView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
         token, created = Token.objects.get_or_create(user=user)
-        
-        # Get user profile data
-        user_serializer = UserSerializer(user)
-        
         return Response({
-            'token': token.key,
-            'user': user_serializer.data
+            "user": UserSerializer(user).data,
+            "token": token.key
         })
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
